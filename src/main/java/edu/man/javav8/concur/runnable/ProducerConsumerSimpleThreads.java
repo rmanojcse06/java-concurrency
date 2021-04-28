9780ff4d519bf1dd6d4c086b58d46e92122e6f39
MRO5COB <Ravikumar.Manoj@in.bosch.com>
2021-04-28 10:00:47 +0530

    simple-producer-consumer

--- src/main/java/edu/man/javav8/concur/runnable/ProducerConsumerSimpleThreads.java
+++ src/main/java/edu/man/javav8/concur/runnable/ProducerConsumerSimpleThreads.java
@@ -1,4 +1,12 @@
 package edu.man.javav8.concur.runnable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.function.Consumer;
+
 class ProductModel implements Comparable<ProductModel>{
 	int id;
 	long createdDt;
@@ -17,28 +25,69 @@ class ProductModel implements Comparable<ProductModel>{
 		return Integer.compare(this.id,o.id);
 	}
 }
-
+class Utils{
+	private static final SimpleDateFormat SDF = new SimpleDateFormat("dd-MMM-YYYY hh:mm:ss,SSS");
+	static Consumer<String> logger(Class clazz) {
+		return (m)->System.out.println("["+SDF.format(new Date(System.currentTimeMillis()))+"] :: "+(null!=clazz?clazz.getName():"")+" - "+m);
+	}
+}
 
 class ProducerOne implements Runnable {
-	public ProducerOne() {}
-
+	Object lock = null;
+	Queue queue = null;
+	Consumer<String> log = null;
+	public ProducerOne(Object lock, Queue queue) {
+		this.lock = lock;
+		this.queue = queue;
+		this.log = Utils.logger(this.getClass());
+	}
+	static int id = 0;
+	
+	
+	@SuppressWarnings("unchecked")
 	public void run() {
-		try {
+		while(true) {
+			try {
+				this.log.accept("Adding into queue");
+				this.queue.add(new ProductModel(++id));
+				this.log.accept("Notifying");
+				synchronized(this.lock) {
+					this.lock.notifyAll();
+				}
+				this.log.accept("Sleeping");
+				Thread.sleep(1000L);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 	}
 }
 
-class ConsumerOne extends Thread {
-	public ConsumerOne() {}
+class ConsumerOne implements Runnable {
+	Object lock = null;
+	Queue queue = null;
+	Consumer<String> log = null;
+	public ConsumerOne(Object lock, Queue queue) {
+		this.lock = lock;
+		this.queue = queue;
+		this.log = Utils.logger(this.getClass());
+	}
 
 	public void run() {
-		try {
-			
-		} catch (Throwable t) {
-			t.printStackTrace();
+		while(true) {
+			try {
+				if(this.queue.isEmpty()) {
+						this.log.accept("waiting");
+						synchronized(this.lock) {
+							this.lock.wait();
+						}
+				}
+				this.log.accept("[ConsumerOne]::consuming model => "+this.queue.remove());
+				Thread.sleep(1);
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
 		}
 	}
 }
@@ -47,9 +96,21 @@ public class ProducerConsumerSimpleThreads {
 
 	public static void main(String[] args) {
 		
-		Thread consumer = new Thread(new ConsumerOne());
+		Object lock = new Object();
+		Queue<ProductModel> queue = new ConcurrentLinkedQueue<>();
+		Thread consumer = new Thread(new ConsumerOne(lock, queue));
 		consumer.start();
-		Thread producer = new Thread(new ProducerOne());
+		
+		Thread consumer2 = new Thread(new ConsumerOne(lock, queue));
+		consumer2.start();
+		
+		Thread producer = new Thread(new ProducerOne(lock, queue));
+		try {
+			producer.join();
+			Thread.sleep(2000L);
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
 		producer.start();
 
 	}
